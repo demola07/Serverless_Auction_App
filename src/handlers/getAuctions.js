b1525f9978e40af1a9245681f89497c453fd7549
Ademola Sobaki <demolasobaki@gmail.com>
2022-07-22 22:53:58 +0100

    implement data validation

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,18 +1,29 @@
 import AWS from 'aws-sdk'
 import commonMiddleware from '../lib/commonMiddleware'
 import createError from 'http-errors'
+import validator from '@middy/validator'
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema'
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 
 async function getAuctions(event, context) {
+	const { status } = event.queryStringParameters
 	let auctions
 
+	const params = {
+		TableName: process.env.AUCTIONS_TABLE_NAME,
+		IndexName: 'statusAndEndDate',
+		KeyConditionExpression: '#status = :status',
+		ExpressionAttributeValues: {
+			':status': status,
+		},
+		ExpressionAttributeNames: {
+			'#status': 'status',
+		},
+	}
+
 	try {
-		const result = await dynamoDB
-			.scan({
-				TableName: process.env.AUCTIONS_TABLE_NAME,
-			})
-			.promise()
+		const result = await dynamoDB.query(params).promise()
 
 		auctions = result.Items
 	} catch (error) {
@@ -26,4 +37,12 @@ async function getAuctions(event, context) {
 	}
 }
 
-export const handler = commonMiddleware(getAuctions)
+export const handler = commonMiddleware(getAuctions).use(
+	validator({
+		inputSchema: getAuctionsSchema,
+		ajvOptions: {
+			useDefaults: true,
+			strict: false,
+		},
+	})
+)
