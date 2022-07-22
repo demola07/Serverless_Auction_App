7bc8a9527d1bc285acef88474575753814da9e74
Ademola Sobaki <demolasobaki@gmail.com>
2022-07-22 01:25:06 +0100

    fix error with deployment of serverless.yaml file, remove dependency not needed

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,11 +1,17 @@
 import { v4 as uuid } from 'uuid'
 import AWS from 'aws-sdk'
+import middy from '@middy/core'
+import httpJsonBodyParser from '@middy/http-json-body-parser'
+import httpEventNormalizer from '@middy/http-event-normalizer'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 
 async function createAuction(event, context) {
-	const { title } = JSON.parse(event.body)
+	const { title } = event.body
 	const now = new Date()
+	let newAuction
 
 	const auction = {
 		id: uuid(),
@@ -14,17 +20,25 @@ async function createAuction(event, context) {
 		createdAt: now.toISOString(),
 	}
 
-	const res = await dynamoDB
-		.put({
-			TableName: process.env.AUCTIONS_TABLE_NAME,
-			Item: auction,
-		})
-		.promise()
+	try {
+		newAuction = await dynamoDB
+			.put({
+				TableName: process.env.AUCTIONS_TABLE_NAME,
+				Item: auction,
+			})
+			.promise()
+	} catch (error) {
+		console.error(error)
+		throw new createError.InternalServerError(error)
+	}
 
 	return {
 		statusCode: 201,
-		body: JSON.stringify(res),
+		body: JSON.stringify(newAuction),
 	}
 }
 
-export const handler = createAuction
+export const handler = middy(createAuction)
+	.use(httpJsonBodyParser())
+	.use(httpEventNormalizer())
+	.use(httpErrorHandler())
